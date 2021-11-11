548815c799f7c634ed6ec71e94c7abd6fd9f3ce8
aranghat <58101741+aranghat@users.noreply.github.com>
2021-11-11 09:31:31 +0530

    Add project files.

---
+++ CapTraining/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main()
+    {
+        //Ctrl + Shift + B Compile Application
+        //Ctrl + F5
+        Console.WriteLine("Hello World !!!!");
+    }
+}
