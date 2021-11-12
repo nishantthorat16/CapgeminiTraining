9b8614af3220852b9569aae28292442b34caaa08
aranghat <58101741+aranghat@users.noreply.github.com>
2021-11-12 11:04:04 +0530

    Added Named Parametes

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -8,23 +8,41 @@ namespace Methods
         {
             int x = 10,y = 10;
 
-            int sum         = Add(x, y);
+            int sum         = Add(firstnumber: x
+                                 ,thirdnumber: y
+                                 ,secondnumber:0);
+
+            Add(x,y,0);
+                                
+                                 
+
             float floatSum  = Add(10.5F, 15.0F);
 
             Console.WriteLine($"{floatSum}");
             Add(10.5, 15.0);
 
-            DisplaySum(x, y, sum);
+            Console.WriteLine($"{x} + {y} = {sum}");
+            //DisplaySum(x, y, sum);
 
+            WithDrawAmount(withdrawAmount: 100, accountBalance:10000);
+
+        }
+
+        static int WithDrawAmount(int accountBalance,int withdrawAmount)
+        {
+            return accountBalance - withdrawAmount;
         }
 
         //return type int
         //Name is Add
         //Parameters a and b
-        static int Add(int a,int b)
+        //B is a optional parameter
+        //Optional parametes should always comes after the default parameters
+        //Cannot have default parameters after optional parameters
+        static int Add(int firstnumber, int secondnumber = 0,int thirdnumber=0)
         {
             Console.WriteLine("Int add");
-            return a + b;
+            return firstnumber + secondnumber + thirdnumber;
         }
 
         static float Add(float a, float b)
@@ -49,5 +67,7 @@ namespace Methods
         {
             Console.WriteLine("Method called at : " + DateTime.Now.ToString());
         }
+
+
     }
 }
