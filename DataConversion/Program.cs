9b75e1f6afcdcc60bb7ee42552dd323030390f72
aranghat <58101741+aranghat@users.noreply.github.com>
2021-11-15 11:00:20 +0530

    Added parse and TryParse

---
+++ DataConversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your fav number : ");
+            var userInput = Console.ReadLine();
+            
+            float favNumber;
+
+            
+
+            if (float.TryParse(userInput, out favNumber))
+                Console.WriteLine($"Thank you for sharing your fav number {favNumber}");
+            else
+                Console.WriteLine("Invalid number: Please enter a vaid digit");
+        }
+    }
+}
