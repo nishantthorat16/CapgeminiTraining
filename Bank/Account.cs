f43898a5db02f63f0f97e19c0634f7a3f1efabeb
aranghat <58101741+aranghat@users.noreply.github.com>
2021-11-18 17:06:04 +0530

    Added  operator overloding

--- Bank/Account.cs
+++ Bank/Account.cs
@@ -8,15 +8,63 @@ namespace Bank
 {
     public abstract class Account
     {
-        public long AccountNumber   { get; set; }
-        public string Name          { get; set; }
-        public string Email         { get; set; }
-       
+        public long AccountNumber { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
         public float AccountBalance { get; protected set; }
-        public float IntrestRate    { get; protected set; }
-        public float MinumumBalance  { get; protected set; }
+        public float IntrestRate { get; protected set; }
+        public float MinumumBalance { get; protected set; }
 
         public abstract void CashDeposit(int depositAmount);
+
+        //Method Overloading
+        public float WithDrawAmount(float amount)
+        {
+            AccountBalance -= amount;
+            return AccountBalance;
+        }
+
+        //Method Overloading
+        public float WithdrawAmount(float amount, int denomination)
+        {
+            AccountBalance -= amount;
+            return AccountBalance;
+        }
+
+        //Method in base class
+        public void DisplayAccountInfo()
+        {
+            Console.WriteLine($"ACC NO : {AccountNumber}\n" +
+                             $"BALANCE : {AccountBalance}");
+        }
+
+        public static Account operator+(Account account1
+                                      , Account account2)
+        {
+            BasicAccount account    = new BasicAccount();
+            account.Name            = account1.Name;
+            account.Email           = account1.Email;
+
+            account.AccountBalance  = account1.AccountBalance 
+                                    + account2.AccountBalance;
+
+            return account;
+        }
+
+        public static Account operator-(Account account1
+                                      , Account account2)
+        {
+            BasicAccount account = new BasicAccount();
+            account.Name = account1.Name;
+            account.Email = account1.Email;
+
+            account.AccountBalance = account1.AccountBalance
+                                    - account2.AccountBalance;
+
+            return account;
+        }
+
     }
 
     public sealed class BasicAccount : Account
@@ -27,12 +75,27 @@ namespace Bank
             MinumumBalance = 10000;
         }
 
+        //Same method name as in base class
+        //Said to HIDE the base class member
+        public new void DisplayAccountInfo()
+        {
+            Console.WriteLine($"ACC NO : {AccountNumber}\n" +
+                             $"BALANCE : {AccountBalance}\n" +
+                             $"NAME    : {Name}");
+        }
+
+        //Method overriding
         public override void CashDeposit(int depositAmount)
         {
             if (depositAmount <= 50000)
                 AccountBalance += depositAmount;
             else
                 Console.WriteLine("You can only deposit up to 50K");
+        }
+
+        public override string ToString()
+        {
+            return $"ACCOUNT NAME : BASIC, ACC NO : {AccountNumber}, ACCOUNT BALANCE :{ AccountBalance}";
         }
     }
 
@@ -50,6 +113,11 @@ namespace Bank
                 AccountBalance += depositAmount;
             else
                 Console.WriteLine("You can only deposit up to 1L");
+        }
+
+        public override string ToString()
+        {
+            return $"ACCOUNT NAME : PREMIUM, ACC NO : {AccountNumber}, ACCOUNT BALANCE :{ AccountBalance}";
         }
     }
 }
--- Bank/Program.cs
+++ Bank/Program.cs
@@ -6,11 +6,37 @@ namespace Bank
     {
         static void Main(string[] args)
         {
+            /*
             BasicAccount account            = new BasicAccount();
             PremiumAccount premiumAccount   = new PremiumAccount();
 
-            Console.WriteLine($"Basic : {account.IntrestRate} , {account.MinumumBalace}");
-            Console.WriteLine($"Premium : {premiumAccount.IntrestRate} , {premiumAccount.MinumumBalace}");
+            Console.WriteLine(account.ToString());
+            Console.WriteLine(premiumAccount.ToString());
+
+            account.DisplayAccountInfo();*/
+
+            //Console.WriteLine($"Basic : {account.IntrestRate} , {account.MinumumBalance}");
+            //Console.WriteLine($"Premium : {premiumAccount.IntrestRate} , {premiumAccount.MinumumBalance}");
+
+            BasicAccount account1 = new BasicAccount();
+            account1.Name = "Sreehari Aranghat";
+            account1.AccountNumber = 10001;
+            account1.CashDeposit(40000);
+
+
+            PremiumAccount account2 = new PremiumAccount();
+            account2.Name           = "Sreehari Aranghat";
+            account2.AccountNumber  = 10002;
+            account2.CashDeposit(90000);
+
+            /*
+                +, -, ==, != , / , *
+             */
+
+            var consolidatdAccount = account1 + account2;
+            var subtractAccount = account1 - account2;
+            Console.WriteLine($"Consolicated Account = {consolidatdAccount.AccountBalance}");
+
         }
     }
 }
