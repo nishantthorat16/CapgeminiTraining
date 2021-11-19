ee14497d47a530d544d0dcc0d25bd6a6b8b922ab
aranghat <58101741+aranghat@users.noreply.github.com>
2021-11-19 15:26:20 +0530

    Added Collections Project

---
+++ GenericCollections/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type saftey
+            //No boxing and unboxing
+            List<int> intList       = new List<int>();
+            List<string> cityName   = new List<string>();
+
+            intList.Add(21);
+            intList.Add(32);
+            intList.AddRange(new int[] { 30, 45, 78, 90 });
+
+            cityName.Add("Bangalore");
+            cityName.Add("California");
+            cityName.AddRange(new string[] { "Washinton DC", "Kolkata", "Trivandrum" });
+
+            intList.Insert(1, 41);
+            intList.Remove(2);
+
+            foreach (var i in intList)
+                Console.WriteLine(i);
+
+            intList.Reverse();
+
+            Console.WriteLine("After reverse");
+            foreach(var i in intList)
+                Console.WriteLine(i);
+
+
+
+            foreach (var city in cityName)
+                Console.WriteLine(city);
+        }
+    }
+}
---
+++ RegularCollections/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace RegularCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] data = new object[10];
+            //Type Saftey : Regular Collection do not have type saftey
+            //Everytime you add a value the collection need to do boxing
+            //Everytime you take value out the system need to perform unboxing
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(10);
+            arrayList.Add(20);
+            arrayList.Add(21);
+            arrayList.Add("Sree");
+            arrayList.Add(20.5F);
+
+            arrayList.Insert(1, 41);
+            foreach (var i in arrayList)
+                Console.WriteLine(i);
+
+            Console.WriteLine($"Total Length of Array is : {arrayList.Count}");
+
+           
+            arrayList.Add(31);
+
+            Console.WriteLine($"Total Length of Array is : {arrayList.Count}");
+
+            arrayList.Remove(10);
+            Console.WriteLine($"Total Length of Array is : {arrayList.Count}");
+        }
+    }
+}
