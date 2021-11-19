5336f31cbe37482fba2e8c5253e070e581d6511c
aranghat <58101741+aranghat@users.noreply.github.com>
2021-11-19 12:04:42 +0530

    Added Chagnes to interface

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -6,6 +6,7 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
+            /*
             IVideoPlayer player = new Phone();
             player.Play("C:\test.mp3");
 
@@ -13,7 +14,42 @@ namespace Interfaces
             tablet.Play("c:\test.mp3");
 
             IVideoPlayer tv = new Tv();
-            tv.Play("c:\test.mp3");
+            tv.Play("c:\test.mp3");*/
+
+            Console.Write("Enter the file name :");
+            string fileName = Console.ReadLine();
+
+            Console.Write("Where do you want to play this : ");
+            string deviceName = Console.ReadLine();
+
+            /*
+            if(deviceName == "Phone")
+            {
+                Phone p = new Phone();
+                p.Play(fileName);
+            }
+            else if(deviceName== "Tablet")
+            {
+                Tablet tablet = new Tablet();
+                tablet.Play(fileName);
+            }*/
+
+            IVideoPlayer player = GetPlayer(deviceName);
+            player.Play(fileName);
+        }
+
+        static IVideoPlayer GetPlayer(string name)
+        {
+            IVideoPlayer player = null;
+
+            switch (name)
+            {
+                case "Phone": player    = new Phone();break;
+                case "Tablet": player   = new Tablet(); break;
+                case "Tv": player       = new Tv(); break;
+            }
+
+            return player;
         }
     }
 }
