0d92e538e6f3c65d4292b29a5ac850a84abe410b
aranghat <58101741+aranghat@users.noreply.github.com>
2021-11-24 13:00:21 +0530

    Added Lock and Threads

---
+++ ThreadLock/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ThreadLock
+{
+    public class Program
+    {
+        static object lockobject = new object();
+        static List<int> numbers = new List<int>();
+        static void Main(string[] args)
+        {
+            //Thread thread1 = new Thread(new ThreadStart(DisplayInformation));
+            //Thread thread2 = new Thread(new ThreadStart(DisplayInformation));
+
+            //thread1.Start();
+            //thread2.Start();
+
+            Thread thread1 = new Thread(new ThreadStart(ProcessNumbes));
+            Thread thread2 = new Thread(new ThreadStart(ProcessNumbes));
+
+            thread1.Start();
+            thread2.Start();
+        }
+
+        public static void DisplayInformation()
+        {
+            lock (lockobject)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} -> {i}");
+                }
+
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Finished Process");
+            }
+        }
+        
+
+        public static void ProcessNumbes()
+        {
+
+            lock (lockobject)
+            {
+                if (numbers.Count == 0)
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        Console.WriteLine(i);
+                        numbers.Add(i);
+                    }
+                }
+
+                if (numbers.Count > 0)
+                {
+                    int totalSum = 0;
+                    foreach (int i in numbers)
+                        totalSum += i;
+
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Total Sum is : " + totalSum);
+                }
+            }
+
+
+        }
+
+
+       
+    }
+}
---
+++ Threading/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxLoopCount = 0;
+            Console.Write("Enter max count : ");
+            maxLoopCount = int.Parse(Console.ReadLine());
+
+            //Thread thread1 = new Thread(new ThreadStart(DisplayNumbers));
+            Thread thread1 = new Thread(new ParameterizedThreadStart(DisplayNumbers));
+            Thread thread2 = new Thread(new ThreadStart(DisplyMessage));
+
+            thread1.Start(maxLoopCount);
+            thread2.Start();
+
+            
+            //DisplyMessage();
+            //DisplayNumbers();
+        }
+
+        public static void DisplayNumbers(object totalCount)
+        {
+            for (int i = 0; i < (int)totalCount; i++)
+            {
+                Console.WriteLine("THREAD 1: " + i);
+                Thread.Sleep(100);
+            }
+        }
+
+        public static void DisplyMessage()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine("THREAD 2 : Downloaing File");
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
