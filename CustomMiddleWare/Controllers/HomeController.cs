449252613cb15f3a3a5d4042cc3e9852d18c4ce1
aranghat <58101741+aranghat@users.noreply.github.com>
2021-11-30 15:40:03 +0530

    Added Middleware

---
+++ CustomMiddleWare/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomMiddleWare.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Action2")]
+        public IActionResult Action2()
+        {
+            return View();
+        }
+
+        [HttpGet("Action3")]
+        public IActionResult Action3()
+        {
+            return View();
+        }
+    }
+}
---
+++ CustomMiddleWare/CustomMiddleWare/LogRequestMiddleWare.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomMiddleWare
+{
+    public class LogRequestMiddleWare
+    {
+        public  RequestDelegate _next;
+
+        public LogRequestMiddleWare(RequestDelegate next)
+        {
+            _next = next;
+            
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            //if (context.Request.Path.Value.Contains("Action2"))
+            //    context.Response.Redirect("/action3");
+            
+
+            File.AppendAllText(@"D:\Capgemini Training\LogRequest.txt"
+                            , $"Recieved request for {context.Request.Path} @ {DateTime.Now}\n");
+            await _next(context);
+        }
+
+    }
+}
